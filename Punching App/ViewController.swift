e8e0b408f4ddece8357e15c3c92ad894e7aa3160
Jennifer Joseph <josephjz@bc.edu>
2020-11-20 23:23:43 -0500

    Completed!

--- Punching App/ViewController.swift
+++ Punching App/ViewController.swift
@@ -6,14 +6,99 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoOrCameraButton: UIButton!
+    
+    var audioPlayer : AVAudioPlayer!
+    var imagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePickerController.delegate = self
     }
+    
+    func playSound(soundName: String) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch { print("could not play sound") }
+        } else { print("could not create audioPlayer from soundName passed in: \(soundName)") }
+    }
+    
+    
+    // LEGGO
+    func showOneButtonAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoButtonPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let photoLibraryAlertAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+            self.accessPhotoLibrary()
+        }
+        let cameraAlertAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+            self.accessCamera()
+        }
+        let cancelAlertAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(photoLibraryAlertAction)
+        alertController.addAction(cameraAlertAction)
+        alertController.addAction(cancelAlertAction)
 
-
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        playSound(soundName: "punchSound")
+        
+        let originalDim = imageView.frame
+        
+        imageView.frame = CGRect(x: imageView.frame.origin.x + 80, y: imageView.frame.origin.y + 80, width: imageView.frame.width - (80*2), height: imageView.frame.height - (80*2))
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0, options: [], animations: {
+            self.imageView.frame = originalDim
+        }, completion: nil)
+    }
 }
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let edittedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = edittedImage
+        } else if let originalImage =  info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = originalImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func accessPhotoLibrary() {
+        // show image picker controller
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func accessCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePickerController.sourceType = .camera
+            present(imagePickerController, animated: true, completion: nil)
+        } else {
+            showOneButtonAlert(title: "No Camera Available", message: "There is no camera available on this device.")
+        }
+        
+    }
+}
+
 
